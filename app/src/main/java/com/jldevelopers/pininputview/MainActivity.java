17e29c621899de7c51b73536593b330d3f92bf68
Home <sanojjack@gmail.com>
2025-07-09 06:10:21 +0530

    Improvements : Added white background to label to give material look.

--- app/src/main/java/com/jldevelopers/pininputview/MainActivity.java
+++ app/src/main/java/com/jldevelopers/pininputview/MainActivity.java
@@ -1,12 +1,15 @@
 package com.jldevelopers.pininputview;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.jldevelopers.pininput.PinInputView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +22,24 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        PinInputView pinInputView = findViewById(R.id.pin_login);
+        pinInputView.setLabel("Enter PIN");
+        pinInputView.setOnPinEnteredListener(new PinInputView.OnPinEnteredListener() {
+            @Override
+            public void onPinEntered(String pin) {
+                Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        PinInputView confirmpin = findViewById(R.id.outlinedTextField);
+        confirmpin.setLabel("Confirm PIN");
+        confirmpin.setOnPinEnteredListener(new PinInputView.OnPinEnteredListener() {
+            @Override
+            public void onPinEntered(String pin) {
+                Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
